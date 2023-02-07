3eaff6433fb22503d0716a9c50baf7c5d93c96d1
Szabolcs Illés <illesszabolcs@gmail.com>
2023-02-07 23:13:43 +0100

    removed unnecessary Aged Brie checks

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -25,7 +25,7 @@ class GildedRose {
             return;
         }
 
-        if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES) && item.quality > 0) {
+        if (!item.name.equals(BACKSTAGE_PASSES) && item.quality > 0) {
             item.quality = item.quality - 1;
         } 
         
@@ -47,7 +47,7 @@ class GildedRose {
             item.quality = item.quality - item.quality;
         }
 
-        if (item.sellIn < 0 && !item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES) && item.quality > 0) {
+        if (item.sellIn < 0 && !item.name.equals(BACKSTAGE_PASSES) && item.quality > 0) {
             item.quality = item.quality - 1;
         } 
     }
