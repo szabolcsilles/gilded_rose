7a33320c281453534c18bf63424c05a04d8bbc31
Szabolcs Illés <illesszabolcs@gmail.com>
2023-02-07 23:17:34 +0100

    moved Backstage Passes logic to single if

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -25,27 +25,29 @@ class GildedRose {
             return;
         }
 
+        if (item.name.equals(BACKSTAGE_PASSES)) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+                if (item.sellIn < 11) {
+                    item.quality = item.quality + 1;
+                }
+
+                if (item.sellIn < 6) {
+                    item.quality = item.quality + 1;
+                }
+            }
+            item.sellIn = item.sellIn - 1;
+            if (item.sellIn < 0 && item.name.equals(BACKSTAGE_PASSES)) {
+                item.quality = item.quality - item.quality;
+            }
+            return;
+        }
+
         if (!item.name.equals(BACKSTAGE_PASSES) && item.quality > 0) {
             item.quality = item.quality - 1;
-        } 
-        
-
-        if (item.name.equals(BACKSTAGE_PASSES) && item.quality < 50) {
-            item.quality = item.quality + 1;
-            if (item.sellIn < 11) {
-                item.quality = item.quality + 1;
-            }
-
-            if (item.sellIn < 6) {
-                item.quality = item.quality + 1;
-            }
         }
 
         item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0 && item.name.equals(BACKSTAGE_PASSES)) {
-            item.quality = item.quality - item.quality;
-        }
 
         if (item.sellIn < 0 && !item.name.equals(BACKSTAGE_PASSES) && item.quality > 0) {
             item.quality = item.quality - 1;
