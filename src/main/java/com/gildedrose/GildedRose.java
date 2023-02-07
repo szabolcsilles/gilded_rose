5d945f08b3bc2558d6080e9536dd7727f8843ce5
Szabolcs Illés <illesszabolcs@gmail.com>
2023-02-07 22:22:06 +0100

    Sulfuras check

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -17,12 +17,13 @@ class GildedRose {
     }
 
     private void updateItem(Item item) {
+        if (item.name.equals(SULFURAS))
+            return;
+            
         if (!item.name.equals(AGED_BRIE)
                 && !item.name.equals(BACKSTAGE_PASSES)) {
             if (item.quality > 0) {
-                if (!item.name.equals(SULFURAS)) {
-                    item.quality = item.quality - 1;
-                }
+                item.quality = item.quality - 1;
             }
         } else {
             if (item.quality < 50) {
@@ -44,17 +45,13 @@ class GildedRose {
             }
         }
 
-        if (!item.name.equals(SULFURAS)) {
-            item.sellIn = item.sellIn - 1;
-        }
+        item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
             if (!item.name.equals(AGED_BRIE)) {
                 if (!item.name.equals(BACKSTAGE_PASSES)) {
                     if (item.quality > 0) {
-                        if (!item.name.equals(SULFURAS)) {
-                            item.quality = item.quality - 1;
-                        }
+                        item.quality = item.quality - 1;
                     }
                 } else {
                     item.quality = item.quality - item.quality;
