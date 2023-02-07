dbad10af31360eb625d9069f4feaf10a27980ad3
Szabolcs Illés <illesszabolcs@gmail.com>
2023-02-07 23:22:31 +0100

    reduced complexity of updateBackstagePasses

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -42,19 +42,21 @@ class GildedRose {
     }
 
     private void updateBackstagePasses(Item item) {
-        if (item.quality < 50) {
+        item.quality = item.quality + 1;
+        if (item.sellIn < 11) {
             item.quality = item.quality + 1;
-            if (item.sellIn < 11) {
-                item.quality = item.quality + 1;
-            }
+        }
 
-            if (item.sellIn < 6) {
-                item.quality = item.quality + 1;
-            }
+        if (item.sellIn < 6) {
+            item.quality = item.quality + 1;
         }
         item.sellIn = item.sellIn - 1;
-        if (item.sellIn < 0 && item.name.equals(BACKSTAGE_PASSES)) {
-            item.quality = item.quality - item.quality;
+        
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+        if (item.quality >= 50) {
+            item.quality = 50;
         }
     }
 
