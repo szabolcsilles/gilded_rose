0c2619494c6fb2838c8cd46f3dad8b1403dc9b8b
Szabolcs Illés <illesszabolcs@gmail.com>
2023-02-07 21:19:02 +0100

    added tests for default item behaviours

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 the well nown Gilded Rose refactoring kata
 
 https://github.com/emilybache/GildedRose-Refactoring-Kata
+# Gilded Rose starting position in Java
+
+## Run the Text Fixture from Command-Line
+
+```
+./gradlew -q text
+```
+
+### Specify Number of Days
+
+For e.g. 10 days:
+
+```
+./gradlew -q text --args 10
+```
---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,42 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class GildedRoseTest {
+
+    private GildedRose createAppWithItem(Item item) {
+        Item[] items = new Item[] {new Item("foo", 0, 0)};
+        return new GildedRose(items);
+    }
+
+    @Test
+    void sellInValueDecreasesOnUpdateQuality() {
+        GildedRose app = createAppWithItem(new Item("foo", 0, 0));
+        app.updateQuality();
+        assertEquals(-1, app.items[0].sellIn);
+    }
+
+    @Test
+    void qualityOfAnItemIsNeverNegative() {
+        GildedRose app = createAppWithItem(new Item("foo", 0, 0));
+        app.updateQuality();
+        assertTrue(0 <= app.items[0].quality);
+    }
+
+    @Test
+    void qualityOfNormalItemDegradesByOneBeforeSellIn() {
+        GildedRose app = createAppWithItem(new Item("foo", 1, 1));
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void qualityOfNormalItemDegradesByTwoAfterSellIn() {
+        GildedRose app = createAppWithItem(new Item("foo", 0, 2));
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+}
